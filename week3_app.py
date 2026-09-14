1f3009a40c948889001345bb71d386ebfcf5019d
jinyiz-max <jinyiz@gwmail.gwu.edu>
2026-09-13 20:19:53 -0400

    Update week3_app.py

--- week3_app.py
+++ week3_app.py
@@ -144,7 +144,7 @@ category_cols = df.select_dtypes(exclude=np.number).columns.tolist()#类别特�
 st.title("Week3_hw: Wheat Seed Measurements: Correlation and PCA")
 st.caption(
     f"{len(df)} observations · {len(numeric_cols)} numeric variables · "
-    "UCI Seeds dataset"
+    "UCI Seeds dataset by Jinyi Zhou"
 )
 #control 控制框的属性
 with st.sidebar:
